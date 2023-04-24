7062e37954c44762d55ac091a3466602d64d5761
Nuno Amaral <n20amaral@outlook.com>
2023-04-24 19:59:10 +0000

    added portfolio info

--- src/Components/Portfolio.jsx
+++ src/Components/Portfolio.jsx
@@ -19,9 +19,9 @@ import React from "react";
  * Need an image? Check out https://unsplash.com to download a photo you
  * freely use on your site.
  */
-import image from "../images/design-desk.jpeg";
+import image from "../images/portfolio.jpg";
 
-const imageAltText = "desktop with books and laptop";
+const imageAltText = "two computers of someone planning a project";
 
 /**
  * Project list
@@ -31,35 +31,33 @@ const imageAltText = "desktop with books and laptop";
  */
 const projectList = [
   {
-    title: "10 Things To Know About Azure Static Web Apps 🎉",
+    title: "Pass by Value or Reference",
     description:
-      "Collaboration to create a beginner friendly article to help explain Azure Static Web Apps and tooling to get started.",
-    url: "https://dev.to/azure/10-things-to-know-about-azure-static-web-apps-3n4i",
+      "Created a presentation to help explain the difference between pass by value and pass by reference in Java.",
+    url: "https://n20amaral.github.io/byval-ref",
   },
   {
-    title: "Web Development for Beginners",
-    description:
-      "Contributed sketch note imagery to accompany each lesson. These help provide visual representation of what is being taught.",
-    url: "https://github.com/microsoft/web-dev-for-beginners",
+    title: "Tim Berners-Lee Tribute Page",
+    description: "Created a tribute page to Tim Berners-Lee, the inventor of the World Wide Web.",
+    url: "https://n20amaral.github.io/berners-lee",
   },
   {
-    title: "My Resume Site",
+    title: "Software Design Antipatterns",
     description:
-      "Created from Microsoft's resume workshop and deployed to GitHub pages. Includes my experience and design abilities.",
-    url: "https://github.com/microsoft/workshop-library/tree/main/full/build-resume-website",
-  },
-  {
-    title: "GitHub Codespaces and github.dev",
-    description:
-      "Video interview to explain when to use GitHub.dev versus GitHub Codespaces, and how best to use each tool.",
-    url: "https://www.youtube.com/watch?v=c3hHhRME_XI",
+      "Created a presentation to help explain the difference between design patterns and antipatterns.",
+    url: "https://n20amaral.github.io/antipatterns",
   },
 ];
 
 const Portfolio = () => {
   return (
     <section className="light" id="portfolio">
-      <h2>Portfolio</h2>
+      <h2>
+        Portfolio using{" "}
+        <a href="https://www.revealjs.com" target="_blank" rel="noreferrer">
+          reveal.js
+        </a>
+      </h2>
       <div style={{ display: "flex", flexDirection: "row", paddingTop: "3rem" }}>
         <div style={{ maxWidth: "40%", alignSelf: "center" }}>
           <img
